4f31a169fc3c459446c443e33d6784787d805869
MJ Section D <n01615449@humber.ca>
2024-07-25 10:37:19 -0400

    redoing sections

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 })
+
+const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in");
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        section.classList.add("fade-out");
+        observer.observe(section);
+    });
